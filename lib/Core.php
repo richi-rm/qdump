cc554ed9852b0550925377ef0f54d39c43954300
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2021-11-28 12:17:18 +0100

    removed Core configuration, added inspection of private and protected properties and methods

--- lib/Core.php
+++ lib/Core.php
@@ -16,80 +16,12 @@ class Core {
 
 
    /**
-    * Configuration of what to add to the output (properties and methods of the
-    * objects)
-    *
-    * @var array
-    */
-   protected $config = [
-      'privm' => null, // private methods
-      'privp' => null, // private properties
-      'protm' => null, // protected methods
-      'protp' => null, // protected properties
-      'pubm'  => null, // public methods
-      'pubp'  => null  // public properties
-   ];
-
-
-   /**
     * Variable to detect cycles in arrays. It is a mark for the arrays being
     * iterated.
     *
     * @var string
     */
    protected $this_array_is_being_iterated = '__vardebug.this_array_is_being_iterated__';
-
-
-   /**
-    * Constructor.
-    *
-    * @param array $config
-    */
-   public function __construct($config)
-   {
-      $this->config = $config;
-   }
-
-
-   /**
-    * It receives an object as a parameter and returns an associative array
-    * with the namespace and the class name.
-    *
-    * @param object $object
-    * @return array
-    */
-   protected function explode_class($object)
-   {
-      $class_namespace = explode('\\', get_class($object));
-      $class_name = array_pop($class_namespace);
-      if (substr($class_name, 0, 15) == 'class@anonymous') {
-         $class_name = 'class@anonymous';
-      }
-      $class_namespace = implode('\\', $class_namespace);
-      return [
-         'class-namespace' => (strlen($class_namespace) > 0 ? $class_namespace . '\\' : ''),
-         'class-name' => $class_name
-      ];
-   }
-
-
-   /**
-    * Returns the file and the line in which the class of the object passed as
-    * a parameter is declared.
-    *
-    * @param $object object
-    * @return string
-    */
-   protected function get_class_file_line($object)
-   {
-      $refl = new \ReflectionClass($object);
-      $file_path = $refl->getFileName();
-      if ($file_path === false) {
-         return '';
-      } else {
-         return $file_path . '(' . $refl->getStartLine() . ')';
-      }
-   }
 
 
    /**
@@ -103,86 +35,96 @@ class Core {
     */
    public function inspect(&$var, $depth = 0)
    {
+      // null
+      //
       if ($var === null) {
          return ['type' => 'null', 'value' => 'null'];
       }
 
+      // bool
+      //
       if (is_bool($var)) {
-         return ['type' => 'boolean', 'value' => ( $var === false ? 'false' : 'true' )];
+         return ['type' => 'bool', 'value' => ( $var === false ? 'false' : 'true' )];
       }
 
+      // int
+      //
       if (is_int($var)) {
-         return ['type' => 'integer', 'value' => strval($var)];
+         return ['type' => 'int', 'value' => strval($var)];
       }
 
+      // float
+      //
       if (is_float($var)) {
          return ['type' => 'float', 'value' => strval($var)];
       }
 
+      // string
+      //
       if (is_string($var)) {
          return ['type' => 'string', 'value' => $var];
       }
 
+      // array
+      //
       if (is_array($var)) {
          $size = count($var);
-         $r = ['type' => 'array', 'size' => $size, 'cycle' => false, 'elements' => []];
+         $r = ['type' => 'array', 'size' => $size];
          if (isset($var[$this->this_array_is_being_iterated])) {
             $r['size'] = $size - 1;
             $r['cycle'] = true;
             return $r;
          }
-         if ($size > 0) {
-            $var[$this->this_array_is_being_iterated] = true;
-            foreach ($var as $array_key => &$array_value) {
-               if ($array_key !== $this->this_array_is_being_iterated) {
-                  $r['elements'][$array_key] = $this->inspect($array_value, $depth + 1);
-               }
-            }
-            unset($var[$this->this_array_is_being_iterated]);
+         if ($size < 1) {
+            return $r;
          }
+         // recursive inspection of elements
+         $var[$this->this_array_is_being_iterated] = true;
+         foreach ($var as $array_key => &$array_value) {
+            if ($array_key !== $this->this_array_is_being_iterated) {
+               $r['elements'][$array_key] = $this->inspect($array_value, $depth + 1);
+            }
+         }
+         unset($var[$this->this_array_is_being_iterated]);
          return $r;
       }
 
+      // object
+      //
       if (is_object($var)) {
-         $class = $this->explode_class($var);
-         $object_id = spl_object_id($var);
-         $r = ['type' => 'object',
-               'class-namespace' => $class['class-namespace'],
-               'class-name' => $class['class-name'],
-               'object-id' => $object_id,
-               'class-file-line' => $this->get_class_file_line($var),
-               'cycle' => false,
-               'properties' => [],
-               'methods' => []
-         ];
-         if (in_array($object_id, array_keys($this->ascending_objects_being_inspected))) {
+         // initial inspection
+         $r = $this->inspect_object($var);
+         if (in_array($r['id'], array_keys($this->ascending_objects_being_inspected))) {
+            unset($r['file(line)']);
+            unset($r['namespace']);
+            unset($r['class']);
+            unset($r['constants']);
+            unset($r['properties']);
+            unset($r['methods']);
             $r['cycle'] = true;
             return $r;
          }
-         if ($this->config['pubm']) {
-            foreach (get_class_methods($var) as $method_name) {
-               $r['methods'][] = [
-                  'access' => 'public',
-                  'name' => $method_name
-               ];
+         // recursive inspection of constants
+         if (isset($r['constants'])) {
+            foreach ($r['constants'] as &$constant) {
+               $constant['value'] = $this->inspect($constant['value'], $depth + 1);
             }
-            sort($r['methods']);
          }
-         if ($this->config['pubp']) {
-            $this->ascending_objects_being_inspected[$object_id] = true;
-            foreach (get_object_vars($var) as $property_name => &$property_value) {
-               $r['properties'][] = [
-                  'access' => 'public',
-                  'name' => $property_name,
-                  'value' => $this->inspect($property_value, $depth + 1)
-               ];
+         // recursive inspection of properties
+         if (isset($r['properties'])) {
+            $this->ascending_objects_being_inspected[$r['id']] = true;
+            foreach ($r['properties'] as &$property) {
+               if (array_key_exists('value', $property)) {
+                  $property['value'] = $this->inspect($property['value'], $depth + 1);
+               }
             }
-            unset($this->ascending_objects_being_inspected[$object_id]);
-            ksort($r['properties']);
+            unset($this->ascending_objects_being_inspected[$r['id']]);
          }
          return $r;
       }
 
+      // resource
+      //
       if (is_resource($var)) {
          return ['type' => 'resource', 'resource-type' => get_resource_type($var)];
          // PHP 8:
@@ -191,6 +133,190 @@ class Core {
          //         'resource-id' => get_resource_id($var)];
       }
 
+      // unknown
+      //
       return ['type' => '(unknown)', 'value' => '(unknown)'];
    }
+
+
+   /**
+    * Inspect an object and return the information found as an array.
+    *
+    * @var object $object
+    * @return array
+    */
+   protected function inspect_object($object)
+   {
+      $r = [
+         'type' => 'object',
+         'file(line)' => null,
+         'namespace' => null,
+         'class' => null,
+         'id' => null
+      ];
+
+      $refl_class = new \ReflectionClass($object);
+
+      // file(line)
+      //
+      $file_path = $refl_class->getFileName();
+      $start_line = $refl_class->getStartLine();
+      $r['file(line)'] = $file_path === false ? null : $file_path . '(' . $start_line . ')';
+
+      // namespace
+      //
+      $namespace = $refl_class->getNamespaceName();
+      $r['namespace'] = $namespace . (strlen($namespace) > 0 ? '\\' : '');
+
+      // class
+      //
+      $class = explode('\\', $refl_class->getName());
+      $class = end($class);
+      if (substr($class, 0, 15) === 'class@anonymous') {
+         $class = 'class@anonymous';
+      }
+      $r['class'] = $class;
+
+      // object id
+      //
+      $r['id'] = spl_object_id($object);
+
+      // constants
+      //
+      foreach ($refl_class->getConstants() as $name => $value) {
+         $refl_constant = new \ReflectionClassConstant($object, $name);
+         if ($refl_constant->isPrivate()) {
+            $access = 'private';
+         } elseif ($refl_constant->isProtected()) {
+            $access = 'protected';
+         } else {
+            $access = 'public';
+         }
+         $r['constants'][] = ['access' => $access, 'name' => $name, 'value' => $value];
+      }
+
+      // properties
+      //
+      foreach ($refl_class->getProperties() as $refl_property) {
+
+         $property = [];
+
+         $refl_property->setAccessible(true);
+
+         // access
+         //
+         if ($refl_property->isPrivate()) {
+            $access = 'private';
+         } elseif ($refl_property->isProtected()) {
+            $access = 'protected';
+         } else {
+            $access = 'public';
+         }
+         $property['access'] = $access;
+
+         // name
+         //
+         $property['name'] = $refl_property->getName();
+
+         // static
+         //
+         if ($refl_property->isStatic()) {
+            $property['static'] = true;
+         }
+
+         // type
+         //
+         if ($refl_property->hasType()) {
+            $property['type'] = [
+               'allows-null' => $refl_property->getType()->allowsNull(),
+               'name' => $refl_property->getType()->getName()
+            ];
+         }
+
+         // value
+         //
+         if ($refl_property->isInitialized($object)) {
+            $property['value'] = $refl_property->getValue($object);
+         } else {
+            $property['uninitialized'] = true;
+         }
+
+         $r['properties'][] = $property;
+      }
+
+      // methods
+      //
+      foreach ($refl_class->getMethods() as $refl_method) {
+
+         $method = [];
+
+         // access
+         //
+         if ($refl_method->isPrivate()) {
+            $access = 'private';
+         } elseif ($refl_method->isProtected()) {
+            $access = 'protected';
+         } else {
+            $access = 'public';
+         }
+         $method['access'] = $access;
+
+         // final
+         //
+         if ($refl_method->isFinal()) {
+            $method['final'] = true;
+         }
+
+         // name
+         //
+         $method['name'] = $refl_method->getName();
+
+         // parameters
+         //
+         $parameters = [];
+         foreach ($refl_method->getParameters() as $refl_parameter) {
+            $parameter = [];
+            if ($refl_parameter->isPassedByReference()) {
+               $parameter['by-reference'] = true;
+            }
+            $parameter['name'] = $refl_parameter->getName();
+            if ($refl_parameter->isOptional()) {
+               $parameter['optional'] = true;
+            }
+            if ($refl_parameter->hasType()) {
+               $parameter['type'] = [
+                  'allows-null' => $refl_parameter->getType()->allowsNull(),
+                  'name' => $refl_parameter->getType()->getName()
+               ];
+            }
+            if ($refl_parameter->isVariadic()) {
+               $parameter['variadic'] = true;
+            }
+            $parameters[] = $parameter;
+         }
+         if (count($parameters) > 0) {
+            $method['parameters'] = $parameters;
+         }
+
+         // static
+         //
+         if ($refl_method->isStatic()) {
+            $method['static'] = true;
+         }
+
+         // type (return type)
+         //
+         if ($refl_method->hasReturnType()) {
+            $method['type'] = [
+               'allows-null' => $refl_method->getReturnType()->allowsNull(),
+               'name' => $refl_method->getReturnType()->getName()
+            ];
+         }
+
+         $r['methods'][] = $method;
+      }
+
+      return $r;
+   }
+
 }
