f37eec65c4744094e7a9ed2ba5f30ea052839991
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2021-12-02 07:08:23 +0100

    changed: deleted suffixes access, constant, constant-name, added suffixes modifier, name, uninitialized

--- lib/Renderer/BasicRenderer/HtmlRenderer.php
+++ lib/Renderer/BasicRenderer/HtmlRenderer.php
@@ -13,19 +13,19 @@ class HtmlRenderer extends BasicRenderer {
 <style type="text/css">
    .vardebug-pre-header    { color: #a0a0a0; background-color: #f0f0f0; padding: 10px; font-size: 11pt; margin: 5px; }
    .vardebug-pre-capture   { color: black; background-color: #f0f0f0; padding: 10px; font-size: 11pt; margin: 5px; }
-   .vardebug-access        { font-style: italic; }
    .vardebug-class         { color: #008000; font-weight: bold; }
-   .vardebug-constant      { color: #008000; }
-   .vardebug-constant-type { font-style: italic; }
    .vardebug-cycle         { color: #ff0000; }
    .vardebug-fileline      { text-decoration: underline; }
    .vardebug-key           { color: #d08000; }
    .vardebug-method        { color: #008000; font-weight: bold; }
+   .vardebug-modifier      { font-style: italic; }
+   .vardebug-name          { color: #008000; }
    .vardebug-namespace     { color: #004000; }
    .vardebug-property      { color: #008000; }
    .vardebug-resource-type { color: #ff00ff; }
    .vardebug-time          { color: #a0a0a0; }
    .vardebug-type          { font-style: italic; }
+   .vardebug-uninitialized { color: #ff0000; }
    .vardebug-unknown       { color: #ff0000; }
    .vardebug-value         { color: #0080ff; font-weight: bold; }
 </style>
@@ -41,19 +41,19 @@ HTML;
       'header'        => '<pre class="vardebug vardebug-pre-header">',
       'capture'       => '<pre class="vardebug vardebug-pre-capture">',
 
-      'access'        => '<span class="vardebug vardebug-access">',
       'class'         => '<span class="vardebug vardebug-class">',
-      'constant'      => '<span class="vardebug vardebug-constant">',
-      'constant-type' => '<span class="vardebug vardebug-constant-type">',
       'cycle'         => '<span class="vardebug vardebug-cycle">',
       'file(line)'    => '<span class="vardebug vardebug-fileline">',
       'key'           => '<span class="vardebug vardebug-key">',
       'method'        => '<span class="vardebug vardebug-method">',
+      'modifier'      => '<span class="vardebug vardebug-modifier">',
+      'name'          => '<span class="vardebug vardebug-name">',
       'namespace'     => '<span class="vardebug vardebug-namespace">',
       'property'      => '<span class="vardebug vardebug-property">',
       'resource-type' => '<span class="vardebug vardebug-resource-type">',
       'time'          => '<span class="vardebug vardebug-time">',
       'type'          => '<span class="vardebug vardebug-type">',
+      'uninitialized' => '<span class="vardebug vardebug-uninitialized">',
       'unknown'       => '<span class="vardebug vardebug-unknown">',
       'value'         => '<span class="vardebug vardebug-value">'
    ];
@@ -68,19 +68,19 @@ HTML;
       'header'        => '</pre>',
       'capture'       => '</pre>',
 
-      'access'        => '</span>',
       'class'         => '</span>',
-      'constant'      => '</span>',
-      'constant-type' => '</span>',
       'cycle'         => '</span>',
       'file(line)'    => '</span>',
       'key'           => '</span>',
       'method'        => '</span>',
+      'modifier'      => '</span>',
+      'name'          => '</span>',
       'namespace'     => '</span>',
       'property'      => '</span>',
       'resource-type' => '</span>',
       'time'          => '</span>',
       'type'          => '</span>',
+      'uninitialized' => '</span>',
       'unknown'       => '</span>',
       'value'         => '</span>'
    ];
