7579e82679926f51f100fe0b9b34f243f2903a7f
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2021-12-10 12:16:16 +0100

    non-composer usage: include

---
+++ lib/include.php
@@ -0,0 +1,20 @@
+<?php
+
+
+include 'StringFormatter/AbstractStringFormatter.php';
+include 'StringFormatter/AsciiFormatter.php';
+include 'StringFormatter/ByteSequenceFormatter.php';
+include 'StringFormatter/ISO88591Formatter.php';
+include 'StringFormatter/JsonFormatter.php';
+include 'StringFormatter/UTF8Formatter.php';
+include 'Renderer/BasicRenderer/BasicRenderer.php';
+include 'Renderer/BasicRenderer/AnsiTextRenderer.php';
+include 'Renderer/BasicRenderer/HtmlCommentRenderer.php';
+include 'Renderer/BasicRenderer/HtmlRenderer.php';
+include 'Renderer/BasicRenderer/PlainTextRenderer.php';
+include 'OutputWriter/OutputWriterInterface.php';
+include 'OutputWriter/FileWriter.php';
+include 'OutputWriter/StdoutWriter.php';
+include 'Context.php';
+include 'Core.php';
+include 'VarDebugger.php';
