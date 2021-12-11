a910498b3a0d6d29deea412e8c85200084083e7e
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2021-12-11 07:55:06 +0100

    include file moved to root folder

---
+++ include.php
@@ -0,0 +1,21 @@
+<?php
+
+
+include 'lib/StringFormatter/AbstractStringFormatter.php';
+include 'lib/StringFormatter/AsciiFormatter.php';
+include 'lib/StringFormatter/ByteSequenceFormatter.php';
+include 'lib/StringFormatter/ISO88591Formatter.php';
+include 'lib/StringFormatter/JsonFormatter.php';
+include 'lib/StringFormatter/UTF8Formatter.php';
+include 'lib/Renderer/BasicRenderer/BasicRenderer.php';
+include 'lib/Renderer/BasicRenderer/AnsiTextRenderer.php';
+include 'lib/Renderer/BasicRenderer/HtmlCommentRenderer.php';
+include 'lib/Renderer/BasicRenderer/HtmlRenderer.php';
+include 'lib/Renderer/BasicRenderer/PlainTextRenderer.php';
+include 'lib/OutputWriter/OutputWriterInterface.php';
+include 'lib/OutputWriter/FileWriter.php';
+include 'lib/OutputWriter/NullWriter.php';
+include 'lib/OutputWriter/StdoutWriter.php';
+include 'lib/Context.php';
+include 'lib/Core.php';
+include 'lib/VarDebugger.php';
